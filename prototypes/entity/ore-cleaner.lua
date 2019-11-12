fc73a8442f928251125c9ac793ca2b69917c722e
Mugiwaxar <mugiwara68@gmail.com>
2019-11-12 18:05:14 +0100

    Ore Cleaner Grapics update

--- prototypes/entity/ore-cleaner.lua
+++ prototypes/entity/ore-cleaner.lua
@@ -6,21 +6,19 @@ ocE.type = "mining-drill"
 ocE.name = "OreCleaner"
 ocE.icon = "__Mobile_Factory__/graphics/icones/OreCleaner.png"
 ocE.icon_size = 32
-ocE.flags = {"placeable-neutral", "player-creation"}
+ocE.flags = {"placeable-neutral", "player-creation", "not-rotatable"}
 ocE.minable = {mining_time = 1, result = "OreCleaner"}
 ocE.max_health = 500
 ocE.resource_categories = {"basic-solid"}
 ocE.corpse = "medium-remnants"
 ocE.mining_speed = 0
-ocE.collision_box = {{ -1.5, -1.5}, {1.5, 1.5}}
-ocE.selection_box = {{ -1.5, -1.5}, {1.5, 1.5}}
+ocE.collision_box = {{ -2.5, -2.5}, {2.5, 1.5}}
+ocE.selection_box = {{ -2.5, -2.5}, {2.5, 1.5}}
 ocE.energy_usage = "1J"
 ocE.resource_searching_radius = 20
 ocE.vector_to_place_result = {0, -1.85}
 ocE.monitor_visualization_tint = {r=78, g=173, b=255}
 ocE.working_sound = nil
-ocE.vehicle_impact_sound = nil
-ocE.shadow_animations = nil
 ocE.input_fluid_patch_sprites = nil
 ocE.input_fluid_patch_shadow_sprites = nil
 ocE.input_fluid_patch_shadow_animations = nil
@@ -29,11 +27,11 @@ ocE.input_fluid_patch_window_flow_sprites = nil
 ocE.input_fluid_patch_window_base_sprites = nil
 ocE.monitor_visualization_tint = nil
 ocE.fast_replaceable_group = nil
-ocE.input_fluid_box = (not data.is_demo) and
+ocE.input_fluid_box =
     {
       pipe_connections =
       {
-        { position = {-2, 0} },
+        { position = {-3.0, 0} },
       }
     }
 ocE.radius_visualization_picture =
@@ -44,13 +42,26 @@ ocE.radius_visualization_picture =
     }
 ocE.animations =
 	{
-		frame_count = 1,
-		filename = "__Mobile_Factory__/graphics/entity/OreCleaner.png",
+		frame_count = 60,
+		filename = "__Mobile_Factory__/graphics/animations/OreCleaner.png",
 		priority = "extra-high",
-		width = 128,
-		height = 128,
-		shift = util.by_pixel(0, 0),
-		scale = 1
+		width = 290,
+		height = 400,
+		animation_speed = 1/3,
+		line_length = 10,
+		shift = {0,-1.25},
+		scale = 0.5
+	}
+ocE.shadow_animations =
+	{
+		frame_count = 1,
+		filename = "__Mobile_Factory__/graphics/animations/OreCleanerShadow.png",
+		priority = "extra-high",
+		width = 200,
+		height = 150,
+		line_length = 1,
+		shift = {1.9,0},
+		scale = 1.35
 	}
 ocE.energy_source =
     {
@@ -58,7 +69,10 @@ ocE.energy_source =
 		emissions_per_minute = 0,
 		usage_priority = "secondary-input",
 		render_no_power_icon = false,
-		render_no_network_icon = false
+		render_no_network_icon = false,
+		buffer_capacity = "5MJ",
+		input_flow_limit = "0J",
+		output_flow_limit = "0J"
 
     }
 ocE.module_specification =
@@ -72,7 +86,7 @@ local ocI = {}
 ocI.type = "item"
 ocI.name = "OreCleaner"
 ocI.icon = "__Mobile_Factory__/graphics/icones/OreCleaner.png"
-ocI.icon_size = 32
+ocI.icon_size = 64
 ocI.place_result = "OreCleaner"
 ocI.subgroup = "DimensionalStuff"
 ocI.order = "g"
@@ -100,7 +114,7 @@ local ocT = {}
 ocT.name = "OreCleaner"
 ocT.type = "technology"
 ocT.icon = "__Mobile_Factory__/graphics/icones/OreCleaner.png"
-ocT.icon_size = 32
+ocT.icon_size = 64
 ocT.unit = {
 	count=15,
 	time=60,
