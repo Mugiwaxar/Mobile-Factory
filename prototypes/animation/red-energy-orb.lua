1046284875a8dbfebb1e45b685fef851a26ea304
Mugiwaxar <mugiwara68@gmail.com>
2019-11-12 21:23:46 +0100

    Ore Cleaner graphics Change

--- prototypes/animation/red-energy-orb.lua
+++ prototypes/animation/red-energy-orb.lua
@@ -7,7 +7,8 @@ reo.frame_count = 240
 reo.filename = "__Mobile_Factory__/graphics/animations/RedEnergyOrb.png"
 reo.size = 100
 reo.line_length = 16
-reo.animation_speed = 1/2
+reo.animation_speed = 1
 reo.scale = 0.65
+reo.flags = {"terrain"}
 
 data:extend{reo}
--- prototypes/beam/OCBeam.lua
+++ prototypes/beam/OCBeam.lua
@@ -5,7 +5,7 @@ ocB.flags = {"not-on-map"}
 ocB.damage_interval = 20
 ocB.random_target_offset = false
 ocB.action_triggered_automatically = false
-ocB.width = 1
+ocB.width = 2
 ocB.direction_count = 1
 
 ocB.head =
@@ -13,28 +13,31 @@ ocB.head =
       filename = "__Mobile_Factory__/graphics/Alpha.png",
       width = 1,
       height = 1,
-	  line_length = 1,
-      frame_count = 20,
-	  animation_speed = 1/5
+	  line_length = 10,
+      frame_count = 10,
+	  animation_speed = 1/15,
+	  scale = 2
     }
 ocB.tail =
     {
       filename = "__Mobile_Factory__/graphics/Alpha.png",
       width = 1,
       height = 1,
-	  line_length = 1,
-      frame_count = 20,
-	  animation_speed = 1/5
+	  line_length = 10,
+      frame_count = 10,
+	  animation_speed = 1/15,
+	  scale = 2
     }
 ocB.body =
     {
       {
       filename = "__Mobile_Factory__/graphics/beams/beam.png",
-      width = 200,
+      width = 10,
       height = 10,
-	  line_length = 1,
-      frame_count = 20,
-	  animation_speed = 1/5
+	  line_length = 10,
+      frame_count = 10,
+	  animation_speed = 1/15,
+	  scale = 2
       }
     }
 data:extend{ocB}
