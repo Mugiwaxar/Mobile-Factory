954da186950fb7e970f32d5998d1b0ada43836fa
Mugiwaxar <mugiwara68@gmail.com>
2019-11-17 10:42:21 +0100

    Laser changes

--- prototypes/beam/OCBeam.lua
+++ prototypes/beam/OCBeam.lua
@@ -1,3 +1,6 @@
+-- ORE CLEANER BEAMS --
+
+-- Collect Beam --
 ocB = {}
 ocB.name = "OCBeam"
 ocB.type = "beam"
@@ -31,7 +34,7 @@ ocB.tail =
 ocB.body =
     {
       {
-      filename = "__Mobile_Factory__/graphics/beams/beam.png",
+      filename = "__Mobile_Factory__/graphics/beams/OCbeam.png",
       width = 10,
       height = 10,
 	  line_length = 10,
@@ -41,3 +44,49 @@ ocB.body =
       }
     }
 data:extend{ocB}
+
+
+-- Send big Beam --
+ocBB = {}
+ocBB.name = "OCBigBeam"
+ocBB.type = "beam"
+ocBB.flags = {"not-on-map"}
+ocBB.damage_interval = 20
+ocBB.random_target_offset = false
+ocBB.action_triggered_automatically = false
+ocBB.width = 5
+ocBB.direction_count = 1
+
+ocBB.head =
+    {
+      filename = "__Mobile_Factory__/graphics/Alpha.png",
+      width = 1,
+      height = 1,
+	  line_length = 16,
+      frame_count = 16,
+	  animation_speed = 1,
+	  scale = 2
+    }
+ocBB.tail =
+    {
+      filename = "__Mobile_Factory__/graphics/Alpha.png",
+      width = 1,
+      height = 1,
+	  line_length = 16,
+      frame_count = 16,
+	  animation_speed = 1,
+	  scale = 2
+    }
+ocBB.body =
+    {
+      {
+      filename = "__Mobile_Factory__/graphics/beams/OCBigbeam.png",
+      width = 8,
+      height = 12,
+	  line_length = 16,
+      frame_count = 16,
+	  animation_speed = 1,
+	  scale = 5
+      }
+    }
+data:extend{ocBB}
