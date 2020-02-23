e9709fb41a306b543df972f758a4e76d287b2a58
Dexy <mugiwara68@gmail.com>
2020-02-23 16:12:05 +0100

    Bugfix

--- migrations/0.0.a105to0.0.106.lua
+++ migrations/0.0.a105to0.0.106.lua
@@ -1,17 +1,14 @@
 for k, MF in pairs(global.MFTable) do
     -- Unlock the Control Center Area --
+    if technologyUnlocked("ControlCenter") then
+		updateFactoryFloorForCC(MF)
+	end
     if technologyUnlocked("ConstructibleArea1") then
         createConstructibleArea1(MF)
     end
     if technologyUnlocked("DeepStorage") then
         createDeepStorageArea(MF)
     end
-    local tilesTable = {}
-	for x = -3, 2 do
-	  for y = -34, -32 do
-		table.insert(tilesTable, {name="refined-hazard-concrete-left",position={x,y}})
-		end
-	end
     if MF.fS ~= nil then
         createTilesSurface(MF.fS, -50, -50, 50, 50, "tutorial-grid")
 	    createTilesSurface(MF.fS, -3, -3, 3, 3, "refined-concrete")
