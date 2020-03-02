893ee524958400788424949fbefd02a720e06a2a
Dexy <mugiwara68@gmail.com>
2020-03-02 05:56:47 +0100

    Added Day/Night cycle

---
+++ migrations/0.0.a108to0.0.109.lua
@@ -0,0 +1,7 @@
+-- Set all Factory surfaces to day/night cycle --
+for k, MF in pairs(global.MFTable) do
+    if MF.fS ~= nil then
+        MF.fS.always_day = false
+        MF.fS.daytime = game.get_surface("nauvis").daytime
+    end
+end
