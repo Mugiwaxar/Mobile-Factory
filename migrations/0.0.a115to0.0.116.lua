a908c1a0a4a4986750135c6c50e04251053467f6
Dexy <mugiwara68@gmail.com>
2020-03-19 19:45:45 +0100

    Loading CrashFix

--- migrations/0.0.a115to0.0.116.lua
+++ migrations/0.0.a115to0.0.116.lua
@@ -7,7 +7,7 @@ for k, MF in pairs(global.MFTable or {}) do
     createTilesSurface(MF.fS, -4, -36, 4, -30, "tutorial-grid")
     createTilesSurface(MF.fS, -3, -34, 3, -32, "refined-hazard-concrete-left")
     -- Recreate the Control Center to Factory TP Area --
-    createTilesSurface(MF.ccS, -2, 4, 2, 8, "tutorial-grid")
+    createTilesSurface(MF.ccS, -3, 4, 3, 8, "tutorial-grid")
     createTilesSurface(MF.ccS, -3, 5, 3, 7, "refined-hazard-concrete-right")
     -- Remove the Accumulator and the Substation inside the Control Center --
     createTilesSurface(MF.ccS, -4, 10, 4, 14, "VoidTile")
@@ -23,7 +23,7 @@ unlockRecipeForAll("MatterInteractor", "MatterSerialization")
 unlockRecipeForAll("FluidInteractor", "DeepTank")
 
 -- Send the Matter Serializer/Printer message --
-if table_size(global.matterSerializerTable) > 0 or table_size(global.matterPrinterTable) > 0 then
+if table_size(global.matterSerializerTable or {}) > 0 or table_size(global.matterPrinterTable or {}) > 0 then
     game.print("#####################################################################################")
     game.print("The 0.0.116 update removed the Matter Serializer/Printer")
     game.print("Now, you have to use the Matter Interactor instead (Configurable with the Tooltip GUI")
