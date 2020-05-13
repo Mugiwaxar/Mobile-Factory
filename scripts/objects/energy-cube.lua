4b445f848eed958b3ad8f3fd016e46645c81b7a3
Dexy <mugiwara68@gmail.com>
2020-05-13 16:34:43 +0200

    Added Auto Balance

--- scripts/objects/energy-cube.lua
+++ scripts/objects/energy-cube.lua
@@ -84,6 +84,10 @@ function EC:update()
 	local spriteNumber = math.ceil(self.ent.energy/self.ent.prototype.electric_energy_source_prototype.buffer_capacity*10)
 	rendering.destroy(self.spriteID)
 	self.spriteID = rendering.draw_sprite{sprite="EnergyCubeMK1Sprite" .. spriteNumber, x_scale=1/7, y_scale=1/7, target=self.ent, surface=self.ent.surface, target_offset={0, -0.3}, render_layer=131}
+
+	-- Balance the Energy with neighboring Cubes --
+	self:balance()
+
 end
 
 
@@ -92,23 +96,93 @@ end
 -- end
 
 
+-- Balance the Energy with neighboring Cubes --
+function EC:balance()
 
+	-- Check the Entity --
+	if self.ent == nil or self.ent.valid == false then return end
 
+	-- Get all Accumulator arount --
+	local ents = self.ent.surface.find_entities_filtered{position=self.ent.position, radius=2, type="accumulator"}
 
+	-- Check all Accumulator --
+	for k, ent in pairs(ents) do
 
+		-- Look for valid Energy Cube --
+		if ent ~= nil and ent.valid == true and ent.name == "EnergyCubeMK1" then
+			local obj = global.entsTable[ent.unit_number]
+			if valid(obj) then
+				if self:energy() > obj:energy() and obj:energy() < obj:maxEnergy() then
+					-- Calcule max flow --
+					local energyVariance = self:energy() - obj:energy()
+					local maxEnergyTranfer = math.min(energyVariance, self:energy(), self:maxOutput()*self.updateTick, obj:maxInput()*self.updateTick)
+					-- Transfer Energy --
+					local transfered = obj:addEnergy(maxEnergyTranfer)
+					-- Remove Energy --
+					self:removeEnergy(transfered)
+				elseif self:energy() < obj:energy() and self:energy() < self:maxEnergy() then
+					-- Calcule max flow --
+					local energyVariance = obj:energy() - self:energy()
+					local maxEnergyTranfer = math.min(energyVariance, obj:energy(), self:maxInput()*self.updateTick, obj:maxOutput()*self.updateTick)
+					-- Transfer Energy --
+					local transfered = self:addEnergy(maxEnergyTranfer)
+					-- Remove Energy --
+					self:removeEnergy(transfered)
+				end
+			end
+		end
+	end
 
+end
 
+-- Return the amount of Energy --
+function EC:energy()
+	if self.ent ~= nil and self.ent.valid == true then
+		return self.ent.energy
+	end
+	return 0
+end
 
+-- Return the Energy Buffer size --
+function EC:maxEnergy()
+	if self.ent ~= nil and self.ent.valid == true then
+		return self.ent.electric_buffer_size
+	end
+	return 1
+end
 
+-- Add Energy (Return the amount added) --
+function EC:addEnergy(amount)
+	if self.ent ~= nil and self.ent.valid == true then
+		local added = math.min(amount, self:maxEnergy() - self:energy())
+		self.ent.energy = self.ent.energy + added
+		return added
+	end
+	return 0
+end
 
+-- Remove Energy (Return the amount removed) --
+function EC:removeEnergy(amount)
+	if self.ent ~= nil and self.ent.valid == true then
+		local removed = math.min(amount, self:energy())
+		self.ent.energy = self.ent.energy - removed
+		return removed
+	end
+	return 0
+end
 
+-- Return the max input flow --
+function EC:maxInput()
+	if self.ent ~= nil and self.ent.valid == true then
+		return self.ent.electric_input_flow_limit
+	end
+	return 0
+end
 
-
-
-
-
-
-
-
-
-
+-- Return the max output flow --
+function EC:maxOutput()
+	if self.ent ~= nil and self.ent.valid == true then
+		return self.ent.electric_output_flow_limit
+	end
+	return 0
+end
