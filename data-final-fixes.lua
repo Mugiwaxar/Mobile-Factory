153005d059c43e60f251710701cb335e5983abb4
Dexy <mugiwara68@gmail.com>
2021-01-03 10:41:19 +0100

    Fixed Lab crash

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,14 +1,14 @@
 -- Highlight Resources --
-for k, ore in pairs(data.raw.resource) do
+for _, ore in pairs(data.raw.resource) do
 	ore.highlight = true
 end
 
 -- Make Dimensional Lab accept all materials --
 if settings.startup["MF-lab-science-packs"].value == "all" then
 	local inputsTable = {}
-	for k, lab in pairs(data.raw.lab) do
+	for _, lab in pairs(data.raw.lab) do
 		if lab.inputs then
-			for k2, name in pairs(lab.inputs) do
+			for _, name in pairs(lab.inputs) do
 				inputsTable[name] = name
 			end
 		end
