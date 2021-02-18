3914d040723ec77c4a639d0adf44e5133f325336
Jacob <jacobwatkins2011+github@gmail.com>
2021-02-18 13:31:13 -0500

    dimensiona-pipe patch for awful mods
    abstract the tinting function and add some safety when a mod edits pipe covers in data

--- prototypes/entity/dimensional-pipe.lua
+++ prototypes/entity/dimensional-pipe.lua
@@ -1,3 +1,36 @@
+---------------------------------------------------------- PIPE FUNCTIONS ------------------------------------------------------------
+local function tintPipeCovers(pipe_covers, tint)
+	--SHOULD NOT HAVE TO DO THIS - hr_version IS ONLY MISSING WHEN A MOD EDITS THE PIPE IN DATA REEEEEEEEEEEE
+	--like Industrial Revolution 2 version 2.1.2
+	if not pipe_covers then return end
+	local directions = {
+		"north",
+		"east",
+		"south",
+		"west",
+	}
+	for _, direction in pairs(directions) do
+		local directionLayer1 = pipe_covers[direction].layers[1]
+		directionLayer1.tint = tint
+		if directionLayer1.hr_version then directionLayer1.hr_version.tint = tint end
+	end
+end
+
+local function tintPictures(pictures, tint)
+	if not pictures then return end
+	local directions = {
+		"up",
+		"right",
+		"down",
+		"left",
+	}
+	for _, direction in pairs(directions) do
+		local pictureDirection = pictures[direction]
+		pictureDirection.tint = tint
+		if pictureDirection.hr_version then pictureDirection.hr_version.tint = tint end
+	end
+end
+
 ---------------------------------------------------------- DIMENSIONAL PIPE ----------------------------------------------------------
 
 -- Tier 1 --
@@ -9,22 +42,8 @@ dpE1.minable = {mining_time = 0.5}
 dpE1.flags = {}
 dpE1.fast_replaceable_group = nil
 dpE1.fluid_box.pipe_connections[2].max_underground_distance = 1
-dpE1.fluid_box.pipe_covers.north.layers[1].tint = tint1
-dpE1.fluid_box.pipe_covers.north.layers[1].hr_version.tint = tint1
-dpE1.fluid_box.pipe_covers.east.layers[1].tint = tint1
-dpE1.fluid_box.pipe_covers.east.layers[1].hr_version.tint = tint1
-dpE1.fluid_box.pipe_covers.south.layers[1].tint = tint1
-dpE1.fluid_box.pipe_covers.south.layers[1].hr_version.tint = tint1
-dpE1.fluid_box.pipe_covers.west.layers[1].tint = tint1
-dpE1.fluid_box.pipe_covers.west.layers[1].hr_version.tint = tint1
-dpE1.pictures.up.tint = tint1
-dpE1.pictures.up.hr_version.tint = tint1
-dpE1.pictures.down.tint = tint1
-dpE1.pictures.down.hr_version.tint = tint1
-dpE1.pictures.left.tint = tint1
-dpE1.pictures.left.hr_version.tint = tint1
-dpE1.pictures.right.tint = tint1
-dpE1.pictures.right.hr_version.tint = tint1
+tintPipeCovers(dpE1.fluid_box.pipe_covers, tint1)
+tintPictures(dpE1.pictures, tint1)
 data:extend{dpE1}
 
 -- Tier 2 --
@@ -33,22 +52,8 @@ local dpE2 = table.deepcopy(dpE1)
 dpE2.name = "DimensionalPipe2"
 dpE2.icons = {{icon=dpE1.icon, tint=tint2}}
 dpE2.fluid_box.base_area = 10
-dpE2.fluid_box.pipe_covers.north.layers[1].tint = tint2
-dpE2.fluid_box.pipe_covers.north.layers[1].hr_version.tint = tint2
-dpE2.fluid_box.pipe_covers.east.layers[1].tint = tint2
-dpE2.fluid_box.pipe_covers.east.layers[1].hr_version.tint = tint2
-dpE2.fluid_box.pipe_covers.south.layers[1].tint = tint2
-dpE2.fluid_box.pipe_covers.south.layers[1].hr_version.tint = tint2
-dpE2.fluid_box.pipe_covers.west.layers[1].tint = tint2
-dpE2.fluid_box.pipe_covers.west.layers[1].hr_version.tint = tint2
-dpE2.pictures.up.tint = tint2
-dpE2.pictures.up.hr_version.tint = tint2
-dpE2.pictures.down.tint = tint2
-dpE2.pictures.down.hr_version.tint = tint2
-dpE2.pictures.left.tint = tint2
-dpE2.pictures.left.hr_version.tint = tint2
-dpE2.pictures.right.tint = tint2
-dpE2.pictures.right.hr_version.tint = tint2
+tintPipeCovers(dpE2.fluid_box.pipe_covers, tint2)
+tintPictures(dpE2.pictures, tint2)
 data:extend{dpE2}
 
 local dpT2 = {}
@@ -76,22 +81,8 @@ local dpE3 = table.deepcopy(dpE2)
 dpE3.name = "DimensionalPipe3"
 dpE3.icons = {{icon=dpE1.icon, tint=tint3}}
 dpE3.fluid_box.base_area = 50
-dpE3.fluid_box.pipe_covers.north.layers[1].tint = tint3
-dpE3.fluid_box.pipe_covers.north.layers[1].hr_version.tint = tint3
-dpE3.fluid_box.pipe_covers.east.layers[1].tint = tint3
-dpE3.fluid_box.pipe_covers.east.layers[1].hr_version.tint = tint3
-dpE3.fluid_box.pipe_covers.south.layers[1].tint = tint3
-dpE3.fluid_box.pipe_covers.south.layers[1].hr_version.tint = tint3
-dpE3.fluid_box.pipe_covers.west.layers[1].tint = tint3
-dpE3.fluid_box.pipe_covers.west.layers[1].hr_version.tint = tint3
-dpE3.pictures.up.tint = tint3
-dpE3.pictures.up.hr_version.tint = tint3
-dpE3.pictures.down.tint = tint3
-dpE3.pictures.down.hr_version.tint = tint3
-dpE3.pictures.left.tint = tint3
-dpE3.pictures.left.hr_version.tint = tint3
-dpE3.pictures.right.tint = tint3
-dpE3.pictures.right.hr_version.tint = tint3
+tintPipeCovers(dpE3.fluid_box.pipe_covers, tint3)
+tintPictures(dpE3.pictures, tint3)
 data:extend{dpE3}
 
 local dpT3 = {}
